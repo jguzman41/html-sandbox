f94c3d84585fc0515e293484284bb100d124f2af
Jonathan Guzman <jguzman41@cnm.edu>
2016-10-11 13:38:05 -0600

    Added section 1

--- public_html/index.php
+++ public_html/index.php
@@ -5,6 +5,22 @@
 		<title> HTML Sandbox</title>
 	</head>
 	<body>
-		<h1>Hello World!</h1>
+		<!-- this is the header-->
+		<header>
+			<h1>Heart attack never stopped old big bear!</h1>
+		</header>
+
+		<!-- this is the main page content-->
+		<main>
+
+			<!-- begin section 1-->
+
+			<section>
+				<p>Hey, maybe you could pop a tent outside with your cousin Maeby… it'd be a good chance to rub off on her. There are dozens of us! DOZENS! So you take your mom to work every day? Bummer. Moms are such a pain in the ass, huh? It's, like, die already!</p>
+					<img src="img/chicken%20dance.gif" alt="chicken dance gif"
+			</section>
+			<section></section>
+			<section></section>
+		</main>
 	</body>
 </html>
