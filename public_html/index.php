14e2fb99851c13caa636fdce4cad7d70a81d0813
Jonathan Guzman <jguzman41@cnm.edu>
2016-10-14 00:53:47 -0600

    updated sandbox

--- public_html/index.php
+++ public_html/index.php
@@ -2,6 +2,9 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
+
+		<!-- add CSS file -->
+		<link href="css/stylesheet.css" rel="stylesheet" type="text/css"  />
 		<title> HTML Sandbox</title>
 	</head>
 	<body>
@@ -16,11 +19,42 @@
 			<!-- begin section 1-->
 
 			<section>
-				<p>Hey, maybe you could pop a tent outside with your cousin Maeby… it'd be a good chance to rub off on her. There are dozens of us! DOZENS! So you take your mom to work every day? Bummer. Moms are such a pain in the ass, huh? It's, like, die already!</p>
+				<p>Hey, maybe you could pop a tent outside with your cousin Maeby… it'd be a good chance to rub off on her. There are dozens of us! DOZENS! I will pack your sweet pink mouth with so much ice cream you'll be the envy of every Jerry and Jane on the block! I made a huge tiny mistake. One of the guys told me to take my head out of my BOTTOM and get back to work…my BOTTOM! Hahahaha. So you take your mom to work every day? Bummer. Moms are such a pain in the ass, huh? It's, like, die already!</p>
 					<img src="img/chicken%20dance.gif" alt="chicken dance gif"
 			</section>
-			<section></section>
-			<section></section>
+
+			<!-- begin section 2-->
+			<section>
+				<h2>More Quotes!</h2>
+				<ul>
+					<li>It seems like only yesterday you were bursting forth from your mother's fertile womb.</li>
+					<li>Everyone's laughing, and riding, and cornholing except Buster.</li>
+					<li>Whenever she'd change clothes, she'd make me wait on the balcony until zip-up, and yet anything goes at bath time</li>
+				</ul>
+			</section>
+
+			<!-- begin section 3-->
+			<section>
+				<table>
+					<tr>
+						<th>Character</th>
+						<th>Quote</th>
+					</tr>
+					<tr>
+						<td>George Michael</td>
+						<td>What a fun, sexy time for you</td>
+					</tr>
+					<tr>
+						<td>Gob</td>
+						<td>Oh, yeah, the guy in the the $4,000 suit is holding the elevator for a guy who doesn't make that in three months. Come on!</td>
+					</tr>
+					</tr>
+					<tr>
+						<td>Tobias</td>
+						<td>Excuse me. Do these effectively hide my thunder?</td>
+					</tr>
+				</table>
+			</section>
 		</main>
 	</body>
 </html>
